2af2f3910d0d811c47620b9ddb54c9fa2aa7601c
Richard Chew <rchew@palantir.com>
2016-11-03 10:22:00 -0700

    Refactored tests to use stateless calculator object

--- tdd-exercises/java/string-calculator/src/test/java/StringCalculatorShould.java
+++ tdd-exercises/java/string-calculator/src/test/java/StringCalculatorShould.java
@@ -9,15 +9,15 @@ import static org.hamcrest.core.Is.is;
 
 public class StringCalculatorShould {
 
+    private StringCalculator calculator = new StringCalculator();
+
     @Test
     public void return_zero_for_empty_string() {
-        StringCalculator calculator = new StringCalculator();
         assertThat(calculator.calculate(""), is(0));
     }
 
     @Test
     public void return_one_for_string_one() {
-        StringCalculator calculator = new StringCalculator();
         assertThat(calculator.calculate("1"), is(1));
     }
 
