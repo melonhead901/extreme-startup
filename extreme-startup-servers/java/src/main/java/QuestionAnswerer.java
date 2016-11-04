ac4e7e5c8af4f3643b354f05cd008df8637d5a83
Richard Chew <rchew@palantir.com>
2016-11-04 16:11:09 -0700

    added behavior for which number is greatest

--- extreme-startup-servers/java/src/main/java/QuestionAnswerer.java
+++ extreme-startup-servers/java/src/main/java/QuestionAnswerer.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionAnswerer {
     public String answer(String query) {
         String out = "";
@@ -9,6 +12,22 @@ public class QuestionAnswerer {
             String question = splitQuery[1].trim();
             if (question.equals("what is your name")) {
                 out = "The Great North";
+            } if (question.contains("which of the following numbers is")) {
+                String numbers = splitQuery[2].trim();
+                String[] numberArgs = splitQuery[2].split(",");
+
+                List<Integer> numberArgsList = new ArrayList<>();
+                for (String num : numberArgs) {
+                    numberArgsList.add(Integer.valueOf(num));
+                }
+
+                if (question.contains("largest")) {
+                    int max = 0;
+                    for (Integer num : numberArgsList) {
+                        max = (num > max) ? num : max;
+                    }
+                    out = Integer.toString(max);
+                }
             }
         } else {
             out = String.format("Unknown query: '%s'", query);
